0f25b9201b8ed5b3d898e3c4e0e7024da8046559
padrewin <97427230+padrewin@users.noreply.github.com>
2024-12-27 19:06:07 +0200

    Create HelpCommand.java

---
+++ src/main/java/dev/padrewin/confirm2Drop/commands/HelpCommand.java
@@ -0,0 +1,42 @@
+package dev.padrewin.confirm2Drop.commands;
+
+import java.util.Collections;
+import java.util.List;
+import dev.padrewin.confirm2Drop.Confirm2Drop;
+import dev.padrewin.confirm2Drop.manager.CommandManager;
+import dev.padrewin.confirm2Drop.manager.LocaleManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permissible;
+
+public class HelpCommand extends BaseCommand {
+
+    private final CommandHandler commandHandler;
+
+    public HelpCommand(CommandHandler commandHandler) {
+        super("help", CommandManager.CommandAliases.HELP);
+        this.commandHandler = commandHandler;
+    }
+
+    @Override
+    public void execute(Confirm2Drop plugin, CommandSender sender, String[] args) {
+        LocaleManager localeManager = plugin.getManager(LocaleManager.class);
+
+        // Send header
+        localeManager.sendMessage(sender, "command-help-title");
+
+        for (NamedExecutor executor : this.commandHandler.getExecutables())
+            if (executor.hasPermission(sender))
+                localeManager.sendSimpleMessage(sender, "command-" + executor.getName() + "-description");
+    }
+
+    @Override
+    public List<String> tabComplete(Confirm2Drop plugin, CommandSender sender, String[] args) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean hasPermission(Permissible permissible) {
+        return true;
+    }
+
+}

463bdd12e91ad3484db2d8fac52910dc11cb467e
padrewin <97427230+padrewin@users.noreply.github.com>
2024-12-27 19:06:17 +0200

    Create ReloadCommand.java

---
+++ src/main/java/dev/padrewin/confirm2Drop/commands/ReloadCommand.java
@@ -0,0 +1,37 @@
+package dev.padrewin.confirm2Drop.commands;
+
+import java.util.Collections;
+import java.util.List;
+import dev.padrewin.confirm2Drop.Confirm2Drop;
+import dev.padrewin.confirm2Drop.manager.CommandManager;
+import dev.padrewin.confirm2Drop.manager.LocaleManager;
+import org.bukkit.command.CommandSender;
+
+public class ReloadCommand extends BaseCommand {
+
+    public ReloadCommand() {
+        super("reload", CommandManager.CommandAliases.RELOAD);
+    }
+
+    @Override
+    public void execute(Confirm2Drop plugin, CommandSender sender, String[] args) {
+        if (!sender.hasPermission("confirm2drop.reload")) {
+            plugin.getManager(LocaleManager.class).sendMessage(sender, "no-permission");
+            return;
+        }
+
+        if (args.length > 0) {
+            plugin.getManager(LocaleManager.class).sendMessage(sender, "command-reload-usage");
+            return;
+        }
+
+        plugin.reloadConfig();
+        plugin.reload();
+        plugin.getManager(LocaleManager.class).sendMessage(sender, "command-reload-success");
+    }
+
+    @Override
+    public List<String> tabComplete(Confirm2Drop plugin, CommandSender sender, String[] args) {
+        return Collections.emptyList();
+    }
+}

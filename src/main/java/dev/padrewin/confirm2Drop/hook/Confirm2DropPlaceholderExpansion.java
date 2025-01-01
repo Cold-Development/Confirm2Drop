18df35c5ceb8cf3ac6ebab2501012293585ca1fd
padrewin <97427230+padrewin@users.noreply.github.com>
2025-01-01 15:41:55 +0200

    Create Confirm2DropPlaceholderExpansion.java

---
+++ src/main/java/dev/padrewin/confirm2Drop/hook/Confirm2DropPlaceholderExpansion.java
@@ -0,0 +1,48 @@
+package dev.padrewin.confirm2Drop.hook;
+
+import dev.padrewin.confirm2Drop.Confirm2Drop;
+import dev.padrewin.confirm2Drop.manager.LocaleManager;
+import org.bukkit.OfflinePlayer;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class Confirm2DropPlaceholderExpansion extends PlaceholderExpansion {
+
+    private final Confirm2Drop confirm2Drop;
+    private final LocaleManager localeManager;
+
+    public Confirm2DropPlaceholderExpansion(Confirm2Drop confirm2Drop) {
+        this.confirm2Drop = confirm2Drop;
+        this.localeManager = this.confirm2Drop.getManager(LocaleManager.class);
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String placeholder) {
+        if (player != null && placeholder.equalsIgnoreCase("toggle_status")) {
+            boolean isEnabled = this.confirm2Drop.getDatabaseManager().getPlayerPreference(player.getUniqueId().toString());
+            return isEnabled
+                    ? localeManager.getLocaleMessage("placeholder-status-enabled")
+                    : localeManager.getLocaleMessage("placeholder-status-disabled");
+        }
+        return null;
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "confirm2drop";
+    }
+
+    @Override
+    public String getAuthor() {
+        return this.confirm2Drop.getDescription().getAuthors().get(0);
+    }
+
+    @Override
+    public String getVersion() {
+        return this.confirm2Drop.getDescription().getVersion();
+    }
+}
